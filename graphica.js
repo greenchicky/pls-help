c94497808c695a0fc1d3b42d0e640b2c0bef766c
greenchicky <99123045+greenchicky@users.noreply.github.com>
2022-02-13 16:50:36 +0300

    Add files via upload

---
+++ graphica.js
@@ -0,0 +1,8 @@
+function mobilenav() {
+  var links = document.getElementById("myLinks");
+  if (links.style.display === "block") {
+    links.style.display = "none";
+  } else {
+    links.style.display = "block";
+  }
+}
